2441fe91462de955deac6354470640aabb755d36
DavideLopez <davidelopez7@gmail.com>
2022-10-11 16:20:48 +0200

    aggiunto controller

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('metaTitle', 'Home Page')
+
+@section('content')
+<ul>
+    @forelse($movies as $movie)
+    <li>
+        <h1>{{ $movie['title'] }}</h1> 
+        <p>{{ $movie['original_title']}}</p>
+        <p>{{ $movie['nationality'] }}</p>
+        <p>{{ $movie['date'] }}</p>
+        <p>{{ $movie['vote'] }}</p>
+    </li>
+
+    @empty
+        <p>NO MOVIE AVAILABLE!</p>
+    @endforelse
+</ul>
+    
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'MoviesController@index');  //viene gestita dal controller: HomeControllers.php
+
