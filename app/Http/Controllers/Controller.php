08ddf55d3cc12f44e4456048786a269b3604ac16
DavideLopez <davidelopez7@gmail.com>
2022-10-11 17:12:50 +0200

    controller fix

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,10 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function index()
+    {
+        $movies = Movie::all();
+        return view('home', compact('movies'));
+    }
 }
